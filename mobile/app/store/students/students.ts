a8b4aece2079213d7a055f682ad71fcaf3df71dd
Flavio A Alvarenga <flavio.fgjj@live.com>
2023-06-12 15:41:40 -0300

    Update students.ts
    mobile students profile changed

--- mobile/app/store/students/students.ts
+++ mobile/app/store/students/students.ts
@@ -3,29 +3,29 @@ export const students = [
   {
     name: 'André Cordeiro',
     rm: '345795',
-    image: require('@assets/andre.jpg'),
+    image: require('../../assets/andre.jpg'),
     description:
       'Desenvolvedor front-end na GFT, trabalha mais de 6 anos na area. Tem um perfil colaborativo e está constantemente em busca de novos conhecimentos, afim de se preparar para enfrentar novos desafios. Com foco em um trabalho objetivo, colaborativo e gerador de resultados.',
   },
   {
     name: 'Danilo Santos',
     rm: '344732',
-    image: require('@assets/danilo.jpg'),
+    image: require('../../assets/danilo.jpg'),
     description:
       'Desenvolvedor, apaixonado por tecnologia, curioso por natureza. Graduado em Engenharia da Computação pela faculdade Anhembi Morumbi, tecnólogo em Sistema para Internet na faculdade Nove de Julho. Dez anos de experiência com desenvolvimento web, construindo e mantendo portais corporativos. MBA em Mobile Development pela FIAP, com foco em desenvolvimento mobile e conceitos de mobile First.',
   },
   {
-    name: 'Teste 2',
-    rm: '12342135',
-    image: require('@assets/flavio.jpg'),
+    name: 'Flávio A Alvarenga',
+    rm: '345141',
+    image: require('../../assets/flavio.jpg'),
     description:
-      'Mussum Ipsum 2, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est.Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.Copo furadis é disculpa de bebadis, arcu quam euismod magna.Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio.',
+      'Trabalhador, apaixonado e motivado, com mais de 8 anos de experiência como Desenvolvedor. Atualmente cursando MBA em Desenvolvimento Mobile na FIAP em São Paulo. E na vida pessoal é casado, pai de dois filhos e tem hobbie praticar esportes e se reunir com amigos para tomar vinho, bater papo e dar risada!',
   },
   {
-    name: 'Teste 3',
-    rm: '1241',
-    image: require('@assets/samuel.jpg'),
+    name: 'Samuel Ribeiro',
+    rm: '344055',
+    image: require('../../assets/samuel.jpg'),
     description:
-      'Mussum Ipsum 3, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est.Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.Copo furadis é disculpa de bebadis, arcu quam euismod magna.Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio.',
+      '24 anos, estou na área de tecnologia trabalhando com desenvolvimento Android a 4 anos e gosto sempre de aprende algo novo, gosto de jogar vídeo game e gosto muito de dormir.',
   },
 ];
